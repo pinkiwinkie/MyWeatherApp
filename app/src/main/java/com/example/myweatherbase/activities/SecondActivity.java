803341b11bea1088ced36cc07fc9ad4b7a94fd49
pinkiwinkie <elrumor99@gmail.com>
2023-05-05 17:59:34 +0200

    ya va el spinner

--- app/src/main/java/com/example/myweatherbase/activities/SecondActivity.java
+++ app/src/main/java/com/example/myweatherbase/activities/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.myweatherbase.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.myweatherbase.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,41 +41,42 @@ public class SecondActivity extends AppCompatActivity {
         buttonForecast = findViewById(R.id.buttonPrevision);
         bAddCity = findViewById(R.id.addCity);
 
-        ArrayAdapter<Ciudad> myAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item,
-                CiudadRepository.getInstance().getAll());
-        spinnerCiudad.setAdapter(myAdapter);
-
         if (savedInstanceState != null) {
             ciudades = (ArrayList<Ciudad>) savedInstanceState.getSerializable("ciudad");
         } else {
-            ciudades = new ArrayList<>();
-            Bundle extras = getIntent().getExtras();
-            if (extras != null) {
-                city = (Ciudad) extras.getSerializable("ciudad");
-                ciudades.add(city);
-                myAdapter.notifyDataSetChanged();
-            }
+            ciudades = new ArrayList<>(CiudadRepository.getInstance().getAll());
         }
+
+        ArrayAdapter<Ciudad> myAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, ciudades);
+        spinnerCiudad.setAdapter(myAdapter);
+
 
         ActivityResultLauncher<Intent> someActivityResultLauncher =
                 registerForActivityResult(
                         new ActivityResultContracts.StartActivityForResult(),
                         result -> {
+                            if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                                Toast.makeText(this, "CANCELLED", Toast.LENGTH_LONG).show();
+                            } else {
+                                Intent data = result.getData();
+                                city = (Ciudad) data.getExtras().getSerializable("ciudad");
+                                ciudades.add(city);
+                                myAdapter.notifyDataSetChanged();
+                            }
                         });
 
         spinnerCiudad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                imageViewCiudad.setImageResource(CiudadRepository.getInstance().getAll().get(i).getImagen());
-                cityOption = CiudadRepository.getInstance().getAll().get(i);
+                imageViewCiudad.setImageResource(ciudades.get(i).getImagen());
+                cityOption = ciudades.get(i);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-
 
 
         buttonForecast.setOnClickListener(new View.OnClickListener() {
@@ -82,9 +88,7 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
-        bAddCity.setOnClickListener(view ->
-
-        {
+        bAddCity.setOnClickListener(view -> {
             Intent intent = new Intent(this, FormularioActivity.class);
             someActivityResultLauncher.launch(intent);
         });
